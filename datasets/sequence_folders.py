8a66d8da340952903b82bbd3604467a642504ca1
Andrei Mihalea <mihalea.andrei@gmail.com>
2020-10-12 02:34:33 +0300

    Changed to png from jpg in sequence folders

--- datasets/sequence_folders.py
+++ datasets/sequence_folders.py
@@ -37,7 +37,8 @@ class SequenceFolder(data.Dataset):
         shifts.pop(demi_length)
         for scene in self.scenes:
             intrinsics = np.genfromtxt(scene/'cam.txt').astype(np.float32).reshape((3, 3))
-            imgs = sorted(scene.files('*.jpg'))
+            # .png instead of .jpg for upb
+            imgs = sorted(scene.files('*.png'))
             if len(imgs) < sequence_length:
                 continue
             for i in range(demi_length, len(imgs)-demi_length):
